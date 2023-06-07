2e09d7e1cddacae91ed8e4269737b2ac3ac7b221
samantariya <samantaislam355@gmail.com>
2023-06-07 23:59:21 +0600

    404 page done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import ExtraSection from "../ExtraSection/ExtraSection";
 import PopularInstructor from "../popularInstructors/PopularInstructor";
@@ -8,6 +9,11 @@ const Home = () => {
       <Banner></Banner>
       <PopularInstructor></PopularInstructor>
       <ExtraSection></ExtraSection>
+      <div className="text-center">
+        <Link className="btn btn-warning" to="/error">
+          Want to Know More?
+        </Link>
+      </div>
     </div>
   );
 };
